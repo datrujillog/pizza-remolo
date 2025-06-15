8fbb1691ca8e42ae492ec1ff0c91909723b0249b
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-06-15 06:09:17 -0500

    feat: agregar rutas para la API de pedidos

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const path = require("path");
 
 const productRoutes = require("./src/routes/productRoutes");
+const orderRoutes = require("./src/routes/orderRoutes");
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "./public/images")));
 
 app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
 
 
 
