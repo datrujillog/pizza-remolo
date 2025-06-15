11e5ebb8812490c262a5f1ccedbeb60eba25cb13
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-06-15 05:06:37 -0500

    feat: agregar middleware para servir imágenes estáticas desde la carpeta pública

--- api/app.js
+++ api/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const productRoutes = require("./src/routes/productRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "./public/images")));
 
 app.use("/api/products", productRoutes);
 
